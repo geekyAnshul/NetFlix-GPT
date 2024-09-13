d4aeeba2c1744aa315f21da750b4530672f968dc
Anshul Namdev <namdevanshul3@gmail.com>
2024-09-13 10:35:35 +0530

    first commit

---
+++ Readme/Readme.txt
@@ -0,0 +1,5 @@
+#Setting Up things
+   npx create-react-app filename
+   npx run start
+   tailwind (get started -> framework guides -> create react -> follow the steps )
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-red-700">
+       Namaste everyone let's build netflix gpt 
+      
+    </div>
+  );
+}
+
+export default App;
